1bee705b0870defa283be132628bc2685c958ae3
xiaoyu zhou <xiao-yu.zhou@ibm.com>
2020-03-17 12:55:11 -0400

    rc1

--- examples/build&install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ..
-
-rm -rf ibm-ibm_zos_zosmf*.tar.gz tests/collections/ansible_collections/ibm | true
-
-ansible-galaxy collection build -f
-
-ansible-galaxy collection install ibm-ibm_zos_zosmf-1.0.0.tar.gz -p tests/collections -f
