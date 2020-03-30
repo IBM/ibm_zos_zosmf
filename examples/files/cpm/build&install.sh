775b919b317be156679e4843d38d4697bf21be8f
xiaoyu zhou <xiaoyuz2011@gmail.com>
2020-03-29 20:15:04 -0400

    Bug fix (#4)
    * Fix
    
    * Fix 1.1
    
    * Fix
    
    * Fix
    
    * Fix remove local instance json file
    
    * Fix remove local instance json file
    
    * Fix linting issue and debug logic in variable parsing
    
    * Fix for typo
    
    * Add print of json fil path to end of provision
    
    * Fix parsing
    
    * Fix json path print to always print
    
    * fix multi line
    
    * Fix double quote
    
    * Fix remove role force remove intance json file even remove api call is failed
    
    Co-authored-by: xiaoyu zhou <xiao-yu.zhou@ibm.com>
    Co-authored-by: Hiren Shah <hiren@us.ibm.com>

--- examples/files/cpm/build&install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ../../..
-
-rm -rf ibm-ibm_zos_zosmf-1.0.0.tar.gz | true
-
-ansible-galaxy collection build -f
-
-ansible-galaxy collection install ibm-ibm_zos_zosmf-1.0.0.tar.gz  -f
