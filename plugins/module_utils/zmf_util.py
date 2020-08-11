642a51bb2e8bc7ef8bc29ff579c3a551fa863f7c
Robyn <yunyyang@cn.ibm.com>
2020-08-11 10:21:41 +0800

    new module: zmf_authenticate

--- plugins/module_utils/zmf_util.py
+++ plugins/module_utils/zmf_util.py
@@ -13,9 +13,9 @@ except Exception:
     LIB_IMP_ERR = traceback.format_exc()
 
 
-def get_connect_argument_spec():
+def get_auth_argument_spec():
     """
-    Return the arguments of ansible module used for session setup.
+    Return the arguments of ansible module used for authentication with z/OSMF server.
     :rtype: dict[str, dict]
     """
     return dict(
@@ -25,6 +25,22 @@ def get_connect_argument_spec():
         zmf_password=dict(required=False, type='str', no_log=True),
         zmf_crt=dict(required=False, type='str', no_log=True),
         zmf_key=dict(required=False, type='str', no_log=True)
+    )
+
+
+def get_connect_argument_spec():
+    """
+    Return the arguments of ansible module used for session setup.
+    :rtype: dict[str, dict]
+    """
+    return dict(
+        zmf_host=dict(required=False, type='str'),
+        zmf_port=dict(required=False, type='int'),
+        zmf_user=dict(required=False, type='str', no_log=True),
+        zmf_password=dict(required=False, type='str', no_log=True),
+        zmf_crt=dict(required=False, type='str', no_log=True),
+        zmf_key=dict(required=False, type='str', no_log=True),
+        zmf_credential=dict(required=False, type='dict', no_log=True)
     )
 
 
@@ -41,14 +57,28 @@ def get_connect_session(module):
     key = module.params['zmf_key']
     user = module.params['zmf_user']
     pw = module.params['zmf_password']
-    if (crt is not None and crt.strip() != '') and (key is not None and key.strip() != ''):
+    auth = None
+    if 'zmf_credential' in module.params:
+        auth = module.params['zmf_credential']
+    if auth is not None and ('LtpaToken2' in auth or 'jwtToken' in auth):
+        cookie = requests.cookies.RequestsCookieJar()
+        if 'LtpaToken2' in auth:
+            cookie.set('LtpaToken2', auth['LtpaToken2'])
+        else:
+            cookie.set('jwtToken', auth['jwtToken'])
+        session.cookies.update(cookie)
+        module.params['zmf_host'] = auth['zmf_host']
+        module.params['zmf_port'] = auth['zmf_port']
+        return session
+    elif (crt is not None and crt.strip() != '') and (key is not None and key.strip() != ''):
         session.cert = (crt.strip(), key.strip())
+        return session
     elif (user is not None and user.strip() != '') and (pw is not None and pw.strip() != ''):
         session.auth = (user.strip(), pw.strip())
+        return session
     else:
         # fail the module since auth is must for zosmf connection
         module.fail_json(msg='HTTP setup error: either zmf_user/zmf_password or zmf_crt/zmf_key are required.')
-    return session
 
 
 def __get_request_headers():
@@ -93,7 +123,10 @@ def handle_request(module, session, method, url, params=None, rcode=200, header=
         else:
             response_content = dict()
         if response_code == rcode:
-            return response_content
+            if '/zosmf/services/authenticate' in url:
+                return dict(response.headers)
+            else:
+                return response_content
         else:
             if 'messageText' in response_content:
                 return 'HTTP request error: ' + str(response_code) + ' : ' + response_content['messageText']
